8cd7c2f15ea0b016e7d6675de608f16025d23751
Ruben Robles <rroblesleal@hotmail.com>
2020-10-06 13:27:01 -0500

    create project

---
+++ Act2.3/Actividad 2.3/Actividad 2.3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Actividad 2.3
+//
+//  Created by Ruben Robles on 10/6/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
